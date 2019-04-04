f9919ba1eb168d90e06484f16c243815ed5fa678
ianzhi <111...aaazz>
2019-04-04 14:47:48 +0800

    修正issue#7问题 1. 更新说明文件 2. 修复issue#7问题

--- plugin/miniprogram/pages/index/index.js
+++ plugin/miniprogram/pages/index/index.js
@@ -1,5 +1,5 @@
 Page({
   data: {
-    html: "<span style='font-family: 'PingFang SC'; color: rgb(51, 51, 51); background: rgb(255, 255, 255);'>我的</span><span style='color: red;font-weight: bold;' class='aaa'>enen</span>"
+    html: "<span style='font-family: 'PingFang SC'; color: rgb(51, 51, 51); background: rgb(255, 255, 255);'>issue#6</span><br/><img src='https://tse3-mm.cn.bing.net/th?id=OIP.tcq8pq08n9Hp-lW_vGsvjgHaE8&w=268&h=179&c=7&o=5&pid=1.7'>issue#7"
   }
 })
--- src/wxparse/config.js
+++ src/wxparse/config.js
@@ -9,7 +9,7 @@ const config = {
   reg: {
     startTag: /^<([-A-Za-z0-9_]+)((?:\s+[a-zA-Z_:][-a-zA-Z0-9_:.]*(?:\s*=\s*(?:(?:"[^=]*")|(?:'[^=]*')|[^>\s]+))?)*)\s*(\/?)>/,
     endTag: /^<\/([-A-Za-z0-9_]+)[^>]*>/,
-    attr: /([a-zA-Z_:][-a-zA-Z0-9_:.]*)(?:\s*=\s*(?:(?:"((?:\\.|[^=])*)")|(?:'((?:\\.|[^=])*)')|([^>\s]+)))?/g
+    attr: /([a-zA-Z_:][-a-zA-Z0-9_:.]*)(?:\s*=\s*(?:(?:"((?:\\.|[^"])*)")|(?:'((?:\\.|[^'])*)')|([^>\s]+)))?/g
   },
   elements: {
     empty: makeMap("area,base,basefont,br,col,frame,hr,img,input,link,meta,param,embed,command,keygen,source,track,wbr"),
